9ad05bf28df539bb8b3bb5c361b0b28e8e94dc08
Mateusz <mateuszhotowy@gmail.com>
2017-12-15 09:40:27 +0100

    Part 2. Done.

--- src/CalculatorTest.java
+++ src/CalculatorTest.java
@@ -2,8 +2,8 @@ public class CalculatorTest {
 
     public static void main(String[] args) {
 
-        Rectangle rectangle = new Rectangle(5,20);
-        Circle circle = new Circle(10);
+        Shape rectangle = new Rectangle(5,20);
+        Shape circle = new Circle(10);
 
         System.out.printf("Pole kwadratu o bokach %.2f i %.2f wynosi %.2f, a jego obwod %.2f%n",
                 rectangle.getA(),rectangle.getB(),ShapeCalculator.calculateArea(rectangle),ShapeCalculator.calculatePerimeter(rectangle));
@@ -13,14 +13,8 @@ public class CalculatorTest {
 
         Shape[] shapes = new Shape[]{rectangle,circle};
         for (Shape shape : shapes) {
-            if (shape instanceof Rectangle){
-                System.out.printf("Pole kwadratu o bokach %.2f i %.2f wynosi %.2f, a jego obwod %.2f%n",
-                        rectangle.getA(),rectangle.getB(),ShapeCalculator.calculateArea(rectangle),ShapeCalculator.calculatePerimeter(rectangle));
-            }
-            else if (shape instanceof Circle){
-                System.out.printf("Pole kola o promieniu %.2f wynosi %.2f, a jego obwod %.2f%n",
-                        circle.getR(),ShapeCalculator.calculateArea(circle),ShapeCalculator.calculatePerimeter(circle));
-            }
+            System.out.printf("Pole tego ksztaltu wynosi %.2f, a jego obwod %.2f%n",
+                    ShapeCalculator.calculateArea(shape),ShapeCalculator.calculatePerimeter(shape));
 
         }
 
--- src/ShapeCalculator.java
+++ src/ShapeCalculator.java
@@ -1,18 +1,50 @@
 public class ShapeCalculator {
 
-    public static double calculateArea(Rectangle r ){
-        return r.getA()*r.getB();
-    }
+    private static double calculateArea(Rectangle r ){ return r.getA()*r.getB(); }
 
-    public static double calculateArea(Circle c){
+    private static double calculateArea(Circle c){
         return Math.PI*Math.pow(c.getR(),2);
     }
 
-    public static double calculatePerimeter(Rectangle r){
+    private static double calculatePerimeter(Rectangle r){
         return r.getA()*2+r.getB()*2;
     }
 
-    public static double calculatePerimeter(Circle c){
-        return 2*Math.PI*c.getR();
+    private static double calculatePerimeter(Circle c){ return 2*Math.PI*c.getR(); }
+
+
+    public static double calculateArea(Shape shape){
+        double result;
+        if (shape instanceof Rectangle){
+            Rectangle r = (Rectangle)shape;
+            result = calculateArea(r);
+        }
+        else if (shape instanceof Circle){
+            Circle c = (Circle)shape;
+            result = calculateArea(c);
+        }
+        else{
+            result = 0;
+        }
+        return result;
     }
+
+    public static double calculatePerimeter(Shape shape){
+        double result;
+        if (shape instanceof Rectangle){
+            Rectangle r = (Rectangle)shape;
+            result = calculatePerimeter(r);
+        }
+        else if (shape instanceof Circle){
+            Circle c = (Circle)shape;
+            result = calculatePerimeter(c);
+        }
+        else{
+            result = 0;
+        }
+        return result;
+    }
+
+
+
 }
